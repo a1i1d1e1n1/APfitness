676692dcfa7ee3506f8734c8e7e5c3a6802ad118
a1i1d1e1n1 <1104906@rgu.ac.uk>
2015-11-18 23:40:21 +0000

    testy

--- public/javascripts/mainCtrls/controllers.js
+++ public/javascripts/mainCtrls/controllers.js
@@ -1,38 +1,57 @@
 ﻿﻿/* global angular */
-(function () {
-    "use strict";
 
     var app = angular.module('App.controllers', []);
 
-    app.controller('homeCtrl', ['$rootScope', '$scope', 'author',
-         function ($rootScope, $scope, author) {
 
-             $scope.getAuthors = function(){
-                var user = {email:"testemail.com", password:"Jacob"};
-                 author.setAll(user).success(function(data) {
-                     var a = data;
-                     var b = 6;
-                 });
+    app.controller('AdminUserCtrl', ['$scope', '$location', '$window', 'UserService', 'AuthenticationService',
+        function($scope, $location, $window, UserService, AuthenticationService) {
+
+            $scope.signIn = function signIn(email, password) {
+                if (email != null && password != null) {
+
+                    UserService.signIn(email, password).success(function(data) {
+                        AuthenticationService.isAuthenticated = true;
+                        $window.sessionStorage.token = data.token;
+                        $location.path("#/userHome");
+                    }).error(function(status, data) {
+                        console.log(status);
+                        console.log(data);
+                    });
                 }
+            };
 
+            $scope.logOut = function logOut() {
+                if (AuthenticationService.isAuthenticated) {
 
-         }
-    ]);
+                    UserService.logOut().success(function(data) {
+                        AuthenticationService.isAuthenticated = false;
+                        delete $window.sessionStorage.token;
+                        $location.path("/");
+                    }).error(function(status, data) {
+                        console.log(status);
+                        console.log(data);
+                    });
+                }
+                else {
+                    $location.path("#/");
+                }
+            };
 
-    app.controller('loginCtrl', ['$rootScope', '$scope', 'author',
-        function ($rootScope, $scope, author) {
-            $scope.user = {};
-            $scope.logIn = function(){
-
-                author.login($scope.user).success(function(data) {
-                    var a = data;
-                    var b = 6;
-                });
-            }
-
-
+            $scope.register = function register(email, password, passwordConfirm) {
+                if (AuthenticationService.isAuthenticated) {
+                    $location.path("#/userHome");
+                }
+                else {
+                    UserService.register(email, password, passwordConfirm).success(function(data) {
+                        $location.path("/admin/login");
+                    }).error(function(status, data) {
+                        console.log(status);
+                        console.log(data);
+                    });
+                }
+            };
         }
     ]);
 
-})();
+
 
