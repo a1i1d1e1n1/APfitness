c8f10fbb3882aac60fb710bf81b5e32b1cd55fdd
a1i1d1e1n1 <1104906@rgu.ac.uk>
2015-11-30 10:31:23 +0000

    created multiple files for the routes

---
+++ routes/routes.js
@@ -0,0 +1,145 @@
+var express = require('express');
+var router = express.Router();
+var mysql      = require('mysql');
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+var secret = require('../config/secret');
+
+var pool  = mysql.createPool({
+  connectionLimit : 10,
+  host     : 'localhost',
+  user     : 'root',
+  password : 'maddog_1',
+  database : 'ApFitness'
+});
+
+var setsalt = function(){
+  return crypto.randomBytes(16).toString('hex');
+};
+
+var getHash = function(password,salt){
+  return crypto.pbkdf2Sync(password, salt, 1000, 64).toString('hex');
+};
+
+var compareHash = function(storedHash,genHash){
+  return storedHash == genHash;
+};
+
+
+router.route('/passwordReset')
+    .get(function(req, res){
+        var email = req.body.email;
+        pool.getConnection(function(err, connection) {
+
+            //Inserts the new user into the database.
+            connection.query('SELECT * from User where email = "' + email +'"', function(err, row) {
+
+                //Returns connection to the pool
+                connection.release();
+
+                if (!err){
+                    if(row[0]){
+                        return res.json("Reset email has been sent to: " + email);
+                    }else{
+                        return res.json("There is no account with that email :-(");
+                    }
+                }
+                else{
+                    return next(err);
+                }
+            });
+        });
+
+    });
+
+
+//Register a users
+router.route('/register')
+    .post(function(req, res, next){
+        //Checks to see if the password and confirm password ma
+        if(req.body.password === req.body.passwordConfirmation){
+            //Gets the email from the request
+            var email = req.body.email;
+
+            //Gets a unique salt value
+            var salt = setsalt();
+
+            //Creates a hash based on the password and unique salt value
+            var hash = getHash(req.body.password,salt);
+
+            pool.getConnection(function(err, connection) {
+
+                //Inserts the new user into the database.
+                connection.query('Insert into user (email,hash,salt,admin,google_user) VALUES (' + connection.escape(email) + ',' + connection.escape(hash) + ',' + connection.escape(salt) + ',true,false)', function (err, rows, fields) {
+
+                    //Returns connection to the pool
+                    connection.release();
+
+                    if (!err){
+                        //Creates a new unique json web token and passes it back to the client.
+                        var token = jwt.sign(email, secret.secretToken, { expiresIn: tokenManager.TOKEN_EXPIRATION_SEC });
+                        return res.json({token:token});
+                    }
+                    else{
+                        return next(err);
+                    }
+                });
+            });
+        }
+    });
+
+router.route('/logout')
+    .get(function(req, res){
+
+        return res.send(200);
+    });
+
+
+
+//login a user to the system.
+router.route('/login')
+    .post(function (req, res) {
+        //Sets the email and password that is passed from the client.
+        var email = req.body.email || '';
+        var password = req.body.password || '';
+
+        //checks to see if the email and password are valid.
+        if (email == '' || password == '') {
+            res.status(401);
+            return res.send('Please enter a email or password');
+        }
+
+        console.log(email +"  "+ password);
+
+        //Get connection to the database
+        pool.getConnection(function(err, connection) {
+            connection.query('SELECT * from User where email = "' + email +'"', function(err, row) {
+                connection.release();
+                if (err) {
+                    console.log('Error while performing Query.');
+                    return res.sendStatus(401);
+                }
+                //Check to see if the email exists in the system.
+                if(!row[0]){
+                    console.log("Attempt failed to login with " + email);
+                    return res.sendStatus(401);
+                }
+
+                //Checks if the password is valid
+                var valid =  compareHash(row[0].hash,getHash(password, row[0].salt));
+
+                if(!valid){
+                    console.log("Password is incorrect " + email);
+                    return res.sendStatus(401);
+                }
+                console.log("Password is correct " + email);
+
+                //Assigns a user a token.
+                var token = jwt.sign(email, secret.secretToken, { expiresIn: 3600 });
+
+                return res.json({token:token});
+            });
+        });
+    });
+
+module.exports = router;
