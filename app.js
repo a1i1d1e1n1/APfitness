cb88ae61a169124e8f6f6fb5dc73cce66444be40
a1i1d1e1n1 <1104906@rgu.ac.uk>
2016-03-23 10:58:00 +0000

    added packages

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var cors = require('cors');
-
 var index = require('./routes/index')
 var system = require('./routes/system');
 var users = require('./routes/users');
